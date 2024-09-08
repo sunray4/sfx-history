cf1b3656e6ac5080f0a9af3a55c5a3c68ea33f55
sunray4 <118241363+sunray4@users.noreply.github.com>
2024-09-07 21:56:40 -0700

    basic working version

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -21,21 +21,32 @@ export default function Content({year}) {
         ["12", "December"],
     ])
     return (
-        <div >
-            {curData.map(({Date, English, Chinese, photos}, index) => (
-                 <div className='flex bg-white opacity-70 m-24 mt-5 mb-10 p-7 rounded rounded-lg drop-shadow-lg transition-transform duration-300 ease-in-out hover:scale-105'>
-                   
-                   <div>
-                    <h3 className='font-custom text-2xl mb-3'>{monthConver.get(Date.substring(0, 2))} {Date.substring(3, 7)}</h3>
-                    <p className='font-custom text-lg mb-3'>{English}</p>
-                    <p className='font-custom text-lg'>{Chinese}</p>
-                    </div>
-                    {photos.map((photo, index) => (
-                        <img className='ml-5 max-w-96 float-right' src={photo} key={index} alt={`Photo from ${monthConver.get(Date.substring(0, 2))} ${Date.substring(3, 7)}`} />
-                    ))}
-                    
-                </div>
-            ))}
+        <div>
+    {curData.map(({ Date, English, Chinese, photos }, index) => (
+        <div
+            key={index}
+            className='flex flex-col md:flex-row bg-white opacity-70 m-10 md:m-20 lg:m-24 p-7 rounded-lg drop-shadow-lg transition-transform duration-300 ease-in-out hover:scale-105'
+        >
+            <div className='flex-1'>
+                <h3 className='font-custom text-2xl mb-3'>
+                    {monthConver.get(Date.substring(0, 2))} {Date.substring(3, 7)}
+                </h3>
+                <p className='font-custom text-lg mb-3'>{English}</p>
+                <p className='font-custom text-lg'>{Chinese}</p>
+            </div>
+            <div className='flex-none flex-shrink-0 ml-5 mt-5 md:mt-0 md:ml-1 lg:ml-4'>
+                {photos.map((photo, index) => (
+                    <img
+                        className='max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl'
+                        src={photo}
+                        key={index}
+                        alt={`Photo from ${monthConver.get(Date.substring(0, 2))} ${Date.substring(3, 7)}`}
+                    />
+                ))}
+            </div>
         </div>
+    ))}
+</div>
+
     )
 }
--- src/components/TimelineNav.jsx
+++ src/components/TimelineNav.jsx
@@ -5,22 +5,24 @@ export default function TimelineNav({handleClick}) {
     {data.map(({Date}, index) => (
         yearSet.add(Date.substring(3, 6))
     ))}
-    // console.log(data)
     const yearArray = Array.from(yearSet);
-    console.log(yearArray)
+    
     return (
     <nav className='flex items-center justify-center m-10 mt-0 drop-shadow-lg'>
-        <span className='text-3xl text-white'><i class="fa-solid fa-angle-down fa-rotate-90"></i></span>       
-        <ul className='flex items-center space-x-5'>
-            <hr className="w-48 h-1 bg-white rounded" />
-            {yearArray.map((year, index) => (
-                <div className='flex items-center space-x-5'>
-                <li key={index} className='text-white text-2xl font-custom drop-shadow-lg'><button name={year} onClick={handleClick}>{year}0</button></li>
-                <hr className="w-48 h-1 bg-white rounded" />
+        <span className='text-3xl text-white'><i className="fa-solid fa-angle-down fa-rotate-90"></i></span>       
+        <ul className='flex items-center space-x-2 md:space-x-4'>
+            
+            {yearArray.map((year, index) => (              
+                <div className='flex items-center space-x-2 md:space-x-4' key={index}>
+                    <hr className="w-5 sm:w-10 md:w-20 lg:w-32 xl:w-40 h-1 bg-white rounded" />
+                    <li className='text-white text-2xl font-custom drop-shadow-lg'>
+                        <button name={year} onClick={handleClick}>{year}0</button>
+                    </li>
                 </div>
             ))}
-            
+            <hr className="w-5 sm:w-10 md:w-20 lg:w-32 xl:w-40 h-1 bg-white rounded" />
         </ul>
-        <span className='text-3xl text-white'><i class="fa-solid fa-angle-down fa-rotate-270"></i></span>
+        
+        <span className='text-3xl text-white'><i className="fa-solid fa-angle-down fa-rotate-270"></i></span>
     </nav>)
 }
