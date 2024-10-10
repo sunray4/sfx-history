64f65f687a199e6dae32ccdf1ee462fa6c971e63
sunray4 <118241363+sunray4@users.noreply.github.com>
2024-10-09 19:51:51 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
         >
           SFX Historical Timeline
         </a> */}
-        <h1 className='m-20 mt-12 mb-14 text-center text-5xl font-extrabold font-custom text-white drop-shadow-lg'>St. Francais Xaiver Parish Historical Timeline</h1>
+        <h1 className='m-20 mt-12 mb-14 text-center text-5xl font-extrabold font-custom text-white drop-shadow-lg'>St. Francis Xaiver Parish Historical Timeline</h1>
       </header>
     );
 }
